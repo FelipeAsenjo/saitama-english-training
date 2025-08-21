bb2c22b9a995ddd97d325e22751fd95ca010186b
Felipe Asenjo <59419221+FelipeAsenjo@users.noreply.github.com>
2025-08-21 12:45:39 -0400

    [feature] Add option +30 to velocityStudy

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -82,6 +82,7 @@ export default function Index() {
                     <option value={5}>5+</option>
                     <option value={6}>6+</option>
                     <option value={7}>7+</option>
+                    <option value={30}>30+</option>
                   </Input>
                 </div>
               </Fade>
